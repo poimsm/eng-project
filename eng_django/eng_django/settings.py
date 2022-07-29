e407b8d3e87584806abaae9bc61bdd493431def0
Oscar Garcia  Fuentes <poimsm@gmail.com>
2022-07-29 16:03:28 -0400

    fix docker

--- eng_django/eng_django/settings.py
+++ eng_django/eng_django/settings.py
@@ -22,8 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
